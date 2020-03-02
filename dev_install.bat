6a82bd8998191b9c02eb73f5f7b9d6d533ee60f1
Dmitriy Dmitriev <dmitriev.dmitry.a@yandex.ru>
2020-03-02 12:42:59 +0300

    added scripts for easier deployment

---
+++ dev_install.bat
@@ -0,0 +1,2 @@
+call .\install.bat
+pip install pylint
---
+++ install.bat
@@ -0,0 +1,6 @@
+pip install virtualenv
+virtualenv venv
+call .\venv\Scripts\activate.bat
+pip install instagram-scraper
+pip install opencv-python
+pip install pillow

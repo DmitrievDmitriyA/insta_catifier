bdc47047621e3204b33cb67afbdaf737fbf04c59
Dmitriy Dmitriev <dmitriev.dmitry.a@yandex.ru>
2020-04-26 00:40:36 +0300

    Modified install script

--- install.bat
+++ install.bat
@@ -1,6 +1,5 @@
+call create_package.bat
 pip install virtualenv
 virtualenv venv --system-site-packages
 call .\venv\Scripts\activate.bat
-pip install instagram-scraper
-pip install opencv-python
-pip install pillow
+pip install .
